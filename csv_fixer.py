79d8700ec44b8879a01650b8fa7bcce0b8abfa6a
William H. Grover <wgrover@gmail.com>
2024-06-26 15:22:27 -0700

    fixed number of commas in empty line

--- csv_fixer.py
+++ csv_fixer.py
@@ -7,7 +7,7 @@ infile = open(sys.argv[1], 'rb')
 content = infile.read()
 infile.close()
 content = content.replace(b'\r\n', b'\n')
-content = content.replace(b',,,\n', b'')
+content = content.replace(b',,\n', b'')
 if not content.endswith(b'\n'):
     content += b'\n'
 outfile = open(sys.argv[1][:-4]+"_FIXED.csv", 'wb')
